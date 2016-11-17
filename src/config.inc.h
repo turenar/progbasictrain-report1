aabcc7329fa547cccc6e9f990c0028651f6737a4
Turenar <sora@turenar.xyz>
2016-11-17 09:47:48 +0900

    add format attribute to logger

--- src/config.inc.h
+++ src/config.inc.h
@@ -4,4 +4,7 @@
 #include "config.h"
 #endif
 
+#ifndef __attribute_format__
+#define __attribute_format__(type, string_index, first_to_check) __attribute((format(type, string_index, first_to_check)))
+#endif
 #define UNUSED_VAR(x) ((void)x)
--- src/logger.h
+++ src/logger.h
@@ -1,8 +1,8 @@
 #pragma once
 
-void log_debug(const char* file, int line, const char* fmt, ...);
-void log_info(const char* file, int line, const char* fmt, ...);
-void log_warn(const char* file, int line, const char* fmt, ...);
-void log_error(const char* file, int line, const char* fmt, ...);
+void log_debug(const char* file, int line, const char* fmt, ...) __attribute_format__(printf, 3, 4);
+void log_info(const char* file, int line, const char* fmt, ...) __attribute_format__(printf, 3, 4);
+void log_warn(const char* file, int line, const char* fmt, ...) __attribute_format__(printf, 3, 4);
+void log_error(const char* file, int line, const char* fmt, ...) __attribute_format__(printf, 3, 4);
 
 #define LOG(level, ...) (log_ ## level (__FILE__, __LINE__, __VA_ARGS__))
