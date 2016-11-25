a174738b5f774389bbbf8d1b603d78d05514ee25
Turenar <sora@turenar.xyz>
2016-11-26 01:26:18 +0900

    cibuild: check c++

--- misc/cibuild.sh
+++ misc/cibuild.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+function _die() {
+	cat $1 >&3
+	exit 1
+}
+
 function check_compile1() {
 	test -d bin/$1 && rm -rf bin/$1
 	mkdir bin/$1
@@ -7,8 +12,8 @@ function check_compile1() {
 	shift
 	exec 3>&1
 	exec >cibuild.log
-	../../configure "$@" 2>&1 || { cat cibuild.log >&3; exit 1; }
-	make check 2>&1 || { cat cibuild.log >&3; exit 1; }
+	../../configure "$@" 2>&1 || _die cibuild.log
+	make check 2>&1 || _die cibuild.log
 	popd >/dev/null 2>&1
 }
 
@@ -23,8 +28,8 @@ function run_job() {
 function check_compile() {
 	name="$1"
 	shift
-	run_job "${name}-debug" $@ --enable-debug
-	run_job "${name}-release" $@
+	run_job "${name}-debug" "$@" --enable-debug
+	run_job "${name}-release" "$@"
 }
 
 function has_cmd() {
@@ -42,7 +47,8 @@ cd "$(dirname "$0")/.."
 if which parallel >/dev/null 2>&1; then
 	test -e .cibuild.pipe && rm .cibuild.pipe
 	mkfifo .cibuild.pipe
-	parallel --progress -v <.cibuild.pipe &
+	parallel --halt=1 --progress -v <.cibuild.pipe &
+	parallel_pid=$!
 	exec 9>.cibuild.pipe
 	rm .cibuild.pipe
 fi
@@ -50,6 +56,8 @@ fi
 #./autogen.sh -v
 test -d bin || mkdir bin
 
+has_cmd x86_64-w64-mingw32-gcc && check_compile cibuild-mingw-x86_64 --host=x86_64-w64-mingw32
+has_cmd i686-w64-mingw32-gcc && check_compile cibuild-mingw-i686 --host=i686-w64-mingw32
 check_compile cibuild-native+psg
 check_compile cibuild-native+sg --without-libpng
 check_compile cibuild-native+pg --without-libsixel
@@ -66,8 +74,10 @@ check_compile cibuild-native+cps  CC=clang --without-gtk3
 check_compile cibuild-native+cs   CC=clang --without-gtk3 --without-libpng
 check_compile cibuild-native+cp   CC=clang --without-gtk3 --without-libsixel
 check_compile cibuild-native+c    CC=clang --without-gtk3 --without-libsixel --without-libpng
-has_cmd x86_64-w64-mingw32-gcc && check_compile cibuild-mingw-x86_64 --host=x86_64-w64-mingw32
-has_cmd i686-w64-mingw32-gcc && check_compile cibuild-mingw-i686 --host=i686-w64-mingw32
+check_compile cibuild-native+xx  CC=g++ CFLAGS='-std=c++11' --without-gtk3 --without-libsixel --without-libpng
 
-exec 9>&-
-wait
+if ${parallel_pid+:} false; then
+	exec 9>&-
+	wait ${parallel_pid}
+	exit $?
+fi
