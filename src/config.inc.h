71636cb923e4c045cc456ba36e99b5bce57e7c61
Turenar <subaru@turenar.xyz>
2016-11-27 00:24:32 +0900

    :checkered_flag:

--- src/config.inc.h
+++ src/config.inc.h
@@ -2,9 +2,15 @@
 
 #ifdef HAVE_CONFIG_H
 #include "config.h"
+#else
+#include "config.vc.h"
 #endif
 
 #ifndef __attribute_format__
+#ifdef __GNUC__
 #define __attribute_format__(type, string_index, first_to_check) __attribute((format(type, string_index, first_to_check)))
+#else
+#define __attribute_format__(a,b,c) /**/
+#endif
 #endif
 #define UNUSED_VAR(x) ((void)x)
