1c79324a1a7949780acdd7b3daa78941a76d31a4
Turenar <sora@turenar.xyz>
2016-11-28 18:48:24 +0900

    :white_check_mark: codec:png:write

--- test/codec/codec_png_test.c
+++ test/codec/codec_png_test.c
@@ -46,12 +46,49 @@ static void test_pbmcodec_png_read(CuTest* tc) {
 	CHECK_PNG_LOAD(PBM_SUCCESS, "png/02_rgb_alpha.png");
 }
 
+static void test_pbmcodec_png_write(CuTest* tc) {
+	pbm_info info;
+	pbm_init(&info);
+	pbm_resize(&info, 3, 3);
+	info.data[0][0] = 0;
+	info.data[0][1] = 1;
+	info.data[0][2] = 1;
+	info.data[1][0] = 1;
+	info.data[1][1] = 0;
+	info.data[1][2] = 0;
+	info.data[2][0] = 1;
+	info.data[2][1] = 1;
+	info.data[2][2] = 0;
+#ifdef _WIN32
+	FILE* fp = fopen("pbm_test.write.tmp", "w+");
+#else
+	FILE* fp = tmpfile();
+#endif
+	CuAssertPtrNotNull(tc, fp);
+	CuAssertIntEquals(tc, PBM_SUCCESS, pbmcodec_png_write(&info, fp));
+	fseek(fp, 0, SEEK_SET);
+	pbmcodec_png_read(&info, fp);
+	CuAssertIntEquals(tc, 3, info.width);
+	CuAssertIntEquals(tc, 3, info.height);
+	CuAssertIntEquals(tc, 0, info.data[0][0]);
+	CuAssertIntEquals(tc, 1, info.data[0][1]);
+	CuAssertIntEquals(tc, 1, info.data[0][2]);
+	CuAssertIntEquals(tc, 1, info.data[1][0]);
+	CuAssertIntEquals(tc, 0, info.data[1][1]);
+	CuAssertIntEquals(tc, 0, info.data[1][2]);
+	CuAssertIntEquals(tc, 1, info.data[2][0]);
+	CuAssertIntEquals(tc, 1, info.data[2][1]);
+	CuAssertIntEquals(tc, 0, info.data[2][2]);
+	pbm_free(&info);
+}
+
 #endif
 
 CuSuite* get_pngcodec_test_suites() {
 	CuSuite* suite = CuSuiteNew();
 #ifdef USE_LIBPNG
 	SUITE_ADD_TEST(suite, test_pbmcodec_png_read);
+	SUITE_ADD_TEST(suite, test_pbmcodec_png_write);
 #endif
 	return suite;
 }
