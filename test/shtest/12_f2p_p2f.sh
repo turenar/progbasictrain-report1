c00fd3ab00c94343baa2a7bfb6e11fb2c260304b
Turenar <sora@turenar.xyz>
2016-11-30 23:19:19 +0900

    :green_heart: :checkered_flag: drop CR

--- test/shtest/12_f2p_p2f.sh
+++ test/shtest/12_f2p_p2f.sh
@@ -1,3 +1,5 @@
 _pass _call -i "${pbmdir}/04_lf.pbm" -t pbm >"${outdir}/12_1.pbm"
 _pass _call -o "${outdir}/12_2.pbm" -f pbm <"${pbmdir}/04_lf.pbm"
-_pass diff -q "${outdir}/12_1.pbm" "${outdir}/12_2.pbm"
+_pass diff -q \
+	<(tr -d '\r' <"${outdir}/12_1.pbm") \
+	<(tr -d '\r' <"${outdir}/12_2.pbm")
