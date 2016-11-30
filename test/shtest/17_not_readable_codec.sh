aeb524189ef212e6214d6737b35ead911169bef3
Turenar <sora@turenar.xyz>
2016-11-30 22:29:41 +0900

    :white_check_mark: add tests for filter:noise, codec:count

---
+++ test/shtest/17_not_readable_codec.sh
@@ -0,0 +1,1 @@
+xfail _call -fcount -tpbm -i"${pbmdir}/04_lf.pbm" -o"${outdir}/17.pbm"
---
+++ test/shtest/30_filter_noise.sh
@@ -0,0 +1,9 @@
+_pass _call -i"${pbmdir}/04_lf.pbm" -o"${outdir}/30_noise1.pbm" noise
+_pass _call -i"${pbmdir}/04_lf.pbm" -o"${outdir}/30_noise2.pbm" noise:0.1
+_pass _call -i"${pbmdir}/04_lf.pbm" -o"${outdir}/30_noise3.pbm" noise:1
+_pass _call -i"${pbmdir}/04_lf.pbm" -o"${outdir}/30_noise4.pbm" noise:0
+xfail _call -i"${pbmdir}/04_lf.pbm" -o"${outdir}/30_noise5.pbm" noise:invalid
+xfail _call -i"${pbmdir}/04_lf.pbm" -o"${outdir}/30_noise5.pbm" noise:
+xfail _call -i"${pbmdir}/04_lf.pbm" -o"${outdir}/30_noise5.pbm" noise:2
+xfail _call -i"${pbmdir}/04_lf.pbm" -o"${outdir}/30_noise5.pbm" noise:-3
+
