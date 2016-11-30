145d390545f3b0ae7018bef7543e34f1011744e3
Turenar <sora@turenar.xyz>
2016-11-30 22:38:35 +0900

    :green_heart: fix outfile name

--- test/shtest/30_filter_noise.sh
+++ test/shtest/30_filter_noise.sh
@@ -3,7 +3,7 @@ _pass _call -i"${pbmdir}/04_lf.pbm" -o"${outdir}/30_noise2.pbm" noise:0.1
 _pass _call -i"${pbmdir}/04_lf.pbm" -o"${outdir}/30_noise3.pbm" noise:1
 _pass _call -i"${pbmdir}/04_lf.pbm" -o"${outdir}/30_noise4.pbm" noise:0
 xfail _call -i"${pbmdir}/04_lf.pbm" -o"${outdir}/30_noise5.pbm" noise:invalid
-xfail _call -i"${pbmdir}/04_lf.pbm" -o"${outdir}/30_noise5.pbm" noise:
-xfail _call -i"${pbmdir}/04_lf.pbm" -o"${outdir}/30_noise5.pbm" noise:2
-xfail _call -i"${pbmdir}/04_lf.pbm" -o"${outdir}/30_noise5.pbm" noise:-3
+xfail _call -i"${pbmdir}/04_lf.pbm" -o"${outdir}/30_noise6.pbm" noise:
+xfail _call -i"${pbmdir}/04_lf.pbm" -o"${outdir}/30_noise7.pbm" noise:2
+xfail _call -i"${pbmdir}/04_lf.pbm" -o"${outdir}/30_noise8.pbm" noise:-3
 
